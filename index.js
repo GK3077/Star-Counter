07a716eab6b76a67fe91805ba0ea5658ab1181ac
GK3077 <gayathrikrishnan.gayu@gmail.com>
2023-01-20 21:39:29 +0530

    Added save button to show previous count

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 function increment(){
@@ -5,5 +6,8 @@ function increment(){
     countEl.innerText = count
 }
 function save(){
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    count = 0
+    countEl.innerText = count
 }
