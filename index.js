706c6ecf67b1100c26b722a76a8bdfb22a33b40f
GK3077 <gayathrikrishnan.gayu@gmail.com>
2023-01-21 20:38:55 +0530

    Enhancing the page

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let countEl = document.getElementById("count-el")
 let count = 0
 function increment(){
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 function save(){
     let countStr = count + " - "
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
     count = 0
-    countEl.innerText = count
+    countEl.textContent = count
 }
