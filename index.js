96d4b89ff877df34a787063be5ec2608b1c77a0d
GK3077 <gayathrikrishnan.gayu@gmail.com>
2023-01-23 11:53:35 +0530

    Added an option to give total no. of stars Counted

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let totalStars = 0
 function increment(){
     count += 1
     countEl.textContent = count
+    totalStars += 1
 }
 function save(){
     let countStr = count + " - "
     saveEl.textContent += countStr
     count = 0
     countEl.textContent = count
+    let totalStr = totalStars 
+    saveEl.textContent += totalStr
 }
